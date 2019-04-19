23553b296a3e5ca05786d676566076cfc9d83eaa
gesown <owner@groganenterpriseservices.com>
2019-04-18 22:26:13 -0700

    Initial commit

--- ClockAngle/Program.cs
+++ ClockAngle/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClockAngle
 {
@@ -10,6 +6,25 @@ namespace ClockAngle
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: ClockAngle Minutes Hour -> Writes angle of separation (degrees).");
+                return;
+                ;
+            }
+            var alpha = 0m; //  hour hand angle in degrees
+            var partHour = 360m / 12m;
+            var delHour = 0m; // fraction of hour
+            var beta = 0m; // minute hand angle in degrees
+            var gamma = 0m; // angle between hands in degrees
+            var minutes = args[0];
+            var hour = args[1];
+            alpha = (decimal.Parse(hour) / 12m) * 360m; // fraction of circle
+
+            beta = (decimal.Parse(minutes) / 60) * 360; // fraction of circle
+            delHour = partHour * beta / 360;
+            gamma = alpha - beta - delHour; // fraction of circle
+            Console.WriteLine("Angle of Separation: " + gamma.ToString("0.00"));
         }
     }
 }
